9209516194879ce1bfccfc020615728cf6bc63e0
Mike Patrick <45206934+michaelspatrick@users.noreply.github.com>
2022-08-05 12:35:10 -0400

    Update sendy-upgrade.sh

--- sendy-upgrade.sh
+++ sendy-upgrade.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-LICENSE="YOUR_LICENSE_HERE"
-SENDY_DIR="/path/to/sendy"
-HTML_DIR="html"
+LICENSE="YOUR_SENDY_LICENSE_HERE"
+SENDY_DIR="/path/to/sendy/installation/directory"
+HTML_DIR="html" # directory name of html files inside SENDY_DIR
+OWNERSHIP="apache:apache"  # Ownership permissions for HTML_DIR in Linux once upgrade is complete
 
 if [ "$(id -u)" != "0" ]; then
-	echo "Sorry, you are not root."
-	exit 1
+        echo "Sorry, you are not root."
+        exit 1
 fi
 
 cd ${SENDY_DIR}
 
 echo "Making a backup of existing Sendy installation..."
-cp -r ${HTML_DIR} ${HTML_DIR}.old
+cp -vr ${HTML_DIR} ${HTML_DIR}.old
 
 echo "Downloading Sendy..."
 wget https://sendy.co/download?license=${LICENSE} -O sendy.zip;
@@ -28,4 +29,6 @@ cp -Rv sendy/* ${HTML_DIR};
 echo "Performing cleanup..."
 rm sendy -drf;
 rm sendy.zip -f;
+rm ${HTML_DIR}.old -drf
+chown ${OWNERSHIP} ${HTML_DIR} -R
 chmod -R ugo-x,u+rwX,go+rX,go-w ${HTML_DIR}/;
